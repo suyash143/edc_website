7a384d28784bb4cd880dca5e50fb7b1067d40672
Shad <shadmirza100@gmail.com>
2022-01-11 16:14:22 +0530

    Carousel added in team section

--- static/index/assets/js/main.js
+++ static/index/assets/js/main.js
@@ -255,3 +255,38 @@
   });
 
 })()
+
+// JQuery Script for Team Carousel
+$('.slider').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,dots: false,
+  prevArrow: false,
+  nextArrow: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
